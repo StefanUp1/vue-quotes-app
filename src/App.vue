217386b45485c9b7a66a33e883e820a0cdc71611
StefanUp1 <stefan_ja95@hotmail.com>
2018-03-19 21:37:18 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <app-info></app-info>
+    </div>
+</template>
+
+<script>
+    import Header from './Components/Header.vue';
+    import NewQuote from './Components/NewQuote.vue';
+    import QuoteGrid from './Components/QuoteGrid.vue';
+    import Info from './Components/Info.vue';
+    
+    
+    export default {
+        data: function() {
+            return {
+                quotes: [
+                    'Just a Quote'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete some Quotes first');
+                }
+                this.quotes.push(quote)
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index,1);
+            }
+        },
+        components: {
+            appHeader: Header,
+            appNewQuote: NewQuote,
+            appQuoteGrid: QuoteGrid,
+            appInfo: Info
+        }
+    }
+</script>
+
+<style>
+</style>
+
+
